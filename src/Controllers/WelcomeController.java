19b93c5027b189e3f86dc0ee210688a3fcbcd857
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-07 23:57:14 +0700

    Add Sign In page to views and WelcomeController

--- src/Controllers/WelcomeController.java
+++ src/Controllers/WelcomeController.java
@@ -1,7 +1,18 @@
 package Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class WelcomeController {
+	@RequestMapping(value = "/index")
+	public String welcome() {
+		
+		return "index";
+	}
 	
+	@RequestMapping(value="signin")
+	public String signin() {
+		return "sign-in";
+	}
 }
