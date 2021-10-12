c187a1549cec18ea2230ad15567234cb78bf2147
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-12 15:02:32 +0700

    Rename file to UserEntity

--- src/Entities/UserEntitty.java
+++
@@ -1,6 +0,0 @@
-package Entities;
-
-
-public class UserEntitty {
-
-}
---
+++ src/Entities/UserEntity.java
@@ -0,0 +1,38 @@
+package Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class UserEntity{
+	@Id
+	@Column(name="Username")
+	private String username;
+	@Column(name="Password")
+	private String password;
+	@Column(name="Fullname")
+	private String fullname;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	
+}
