06df1de41ea5efa0bcbb1572e22326ad0811f326
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-12 14:55:28 +0700

    Add mapping to dashboard page

---
+++ src/Controllers/DashboardController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+	@RequestMapping("/index")
+	public String renderAdminPage() {
+		return "index";
+	}
+}
