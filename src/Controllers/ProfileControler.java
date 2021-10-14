295291d849d1a7c7b7ad74681a4b7dc40d4b8b6a
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-14 08:14:59 +0700

    Rename package Controllers to controllers

--- src/Controllers/ProfileControler.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/profile/")
-public class ProfileControler {
-}
