a19cb6011706f5de8e47be142e567591845c762b
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-14 08:15:31 +0700

    Rename package Controllers to controller

---
+++ src/controllers/DashboardController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/")
+public class DashboardController {
+	@RequestMapping("dashboard")
+	public String renderAdminPage() {
+		return "admin/pages/dashboard";
+	}
+	
+	@RequestMapping("")
+	public String renderPage() {
+		return "";
+	}
+}
---
+++ src/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/")
+public class ProductController {
+	@RequestMapping("product")
+	public String renderProductPage() {
+		return "admin/pages/product";
+	}
+	
+	
+}
