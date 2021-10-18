aa1c6e2fb1bc4e2f2d77d9c35224984de2651c1b
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-18 23:00:52 +0700

    Add title attribute for 400 and 500 page

--- src/controllers/ErrorController.java
+++ src/controllers/ErrorController.java
@@ -1,19 +1,31 @@
 package controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/admin/")
 public class ErrorController {
-	@RequestMapping("404")
-	public String render404Page() {
+
+	String viewsDirectory = "admin/error/";
+
+	@RequestMapping("400")
+	public String render404Page(ModelMap model, HttpServletRequest request) {
+		model.addAttribute("title", "404 - Page Not Found!");
 		System.out.println("404 page");
-		return "admin/error/404";
+		return viewsDirectory + "404";
 	}
-	
+
 	@RequestMapping("500")
-	public String render500Page() {
-		return "admin/error/500";
+	public String render500Page(ModelMap model) {
+		model.addAttribute("title", "500 – Internal Server Error!");
+		return viewsDirectory + "500";
 	}
+
 }
