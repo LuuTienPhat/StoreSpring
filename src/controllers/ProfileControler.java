396164d9e1babb9bcf6831675b351a3557cffd58
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-22 10:59:24 +0700

    Create ProfileController

--- src/controllers/ProfileControler.java
+++ src/controllers/ProfileControler.java
@@ -2,8 +2,21 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/profile/")
+@RequestMapping("/admin/profile")
 public class ProfileControler {
+	
+	String viewsDirectory = "admin/pages/profile/";
+	
+	@RequestMapping("")
+	public String renderProfilePage() {
+		return viewsDirectory + "profile";
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST )
+	public String editProfile() {
+		return viewsDirectory + "profile";
+	}
 }
