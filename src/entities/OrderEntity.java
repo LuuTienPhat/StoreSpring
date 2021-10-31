b032b1e6014e008a1e82e3964a6271c2ae88b070
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-31 23:49:31 +0700

    Add column order_date to the entity and class members getter/setter

--- src/entities/OrderEntity.java
+++ src/entities/OrderEntity.java
@@ -21,6 +21,8 @@ public class OrderEntity {
 	private String id;
 	@Column(name = "customer_id")
 	private String customerId;
+	@Column(name = "order_total")
+	private float orderTotal;
 	@Column(name = "order_date")
 	private Date orderDate;
 	@Column(name = "shipped_date")
@@ -43,4 +45,100 @@ public class OrderEntity {
 	@JoinColumn(name = "customer_id", insertable = false, updatable = false)
 	private CustomerEntity customer;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Date getShippedDate() {
+		return shippedDate;
+	}
+
+	public void setShippedDate(Date shippedDate) {
+		this.shippedDate = shippedDate;
+	}
+
+	public String getShipName() {
+		return shipName;
+	}
+
+	public void setShipName(String shipName) {
+		this.shipName = shipName;
+	}
+
+	public String getShipPhone() {
+		return shipPhone;
+	}
+
+	public void setShipPhone(String shipPhone) {
+		this.shipPhone = shipPhone;
+	}
+
+	public String getShipAddress() {
+		return shipAddress;
+	}
+
+	public void setShipAddress(String shipAddress) {
+		this.shipAddress = shipAddress;
+	}
+
+	public String getShipCity() {
+		return shipCity;
+	}
+
+	public void setShipCity(String shipCity) {
+		this.shipCity = shipCity;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public List<OrderDetailEntity> getOrderDetails() {
+		return orderDetails;
+	}
+
+	public void setOrderDetails(List<OrderDetailEntity> orderDetails) {
+		this.orderDetails = orderDetails;
+	}
+
+	public CustomerEntity getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(CustomerEntity customer) {
+		this.customer = customer;
+	}
+
+	public float getOrderTotal() {
+		return orderTotal;
+	}
+
+	public void setOrderTotal(float orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+	
 }
