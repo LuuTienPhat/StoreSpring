f9bc80602bf89a6d696ea6722e672dff50618a25
tuanbuinov09 <86548995+tuanbuinov09@users.noreply.github.com>
2021-11-02 02:55:14 +0700

    add pagedListHolder for category views

--- src/controllers/GiftController.java
+++ src/controllers/GiftController.java
@@ -15,18 +15,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import entities.CartDetailEntity;
+import entities.CategoryEntity;
 import entities.CustomerEntity;
 import entities.ProductEntity;
+import models.CustomerLoginAccountModel;
 import models.CustomerValidateModel;
 
 @Transactional
@@ -39,48 +43,65 @@ public class GiftController {
 	@RequestMapping("")
 	public String store(ModelMap model) {
 		model.addAttribute("list", getListProduct());
+		model.addAttribute("listCategory", getListCategory());
 		return "store/index";
 	}
 
 	@RequestMapping("/")
 	public String store2(ModelMap model) {
 		model.addAttribute("list", getListProduct());
+		model.addAttribute("listCategory", getListCategory());
 		return "store/index";
-	}
-
-	@RequestMapping("/product-detail")
-	public String productDetail() {
-		return "store/product-detail";
 	}
 
 	@RequestMapping("/product-detail/{productId}")
 	public String productDetail2(@PathVariable("productId") String productId, ModelMap model) {
 		model.addAttribute("product", getProduct(productId));
+		model.addAttribute("listCategory", getListCategory());
 		System.out.println(productId + "; " + getProduct(productId).getName());
 		return "store/product-detail";
 	}
 
-	@RequestMapping("/sign-in")
-	public String signIn() {
+	@RequestMapping(value = "/sign-in")
+	public String signIn(ModelMap model) {
+		model.addAttribute("account", new CustomerLoginAccountModel());
+		model.addAttribute("listCategory", getListCategory());
 		return "store/sign-in";
+	}
+
+	@RequestMapping(value = "/sign-in", method = RequestMethod.POST)
+	public String signIn(@ModelAttribute("account") CustomerLoginAccountModel account, ModelMap model, BindingResult errors) {
+		model.addAttribute("listCategory", getListCategory());
+		if(!isCustomerWithUsernameExit(account.getUsername())) {
+			errors.rejectValue("username", "account", "Tên đăng nhập không tồn tại!");
+		}else if(!account.getPassword().equals(getPasswordOfCustomerWithUsername(account.getUsername()))) {
+			errors.rejectValue("username", "password", "Mật khẩu không đúng!");
+		}
+		if (errors.hasErrors()) {
+//			model.addAttribute("message", "Vui lòng sửa các lỗi sau đây!");
+			return "store/sign-in";
+		} else {
+		return "redirect:/store";}
 	}
 
 	@RequestMapping("/sign-up")
 	public String signUp(ModelMap model) {
 		System.out.println(createTheNextCustomerId());
 		model.addAttribute("customer", new CustomerValidateModel());
+		model.addAttribute("listCategory", getListCategory());
 		return "store/sign-up";
 	}
 
 	@RequestMapping(value = "/sign-up", method = RequestMethod.POST)
 	public String signUp(ModelMap model, @ModelAttribute("customer") CustomerValidateModel customer,
 			BindingResult errors) {
+		model.addAttribute("listCategory", getListCategory());
 
 		if (customer.getUsername().trim().length() == 0) {
 			errors.rejectValue("username", "customer", "Vui lòng nhập tên đăng nhập!");
 		} else if (customer.getUsername().trim().length() < 4) {
 			errors.rejectValue("username", "customer", "Tên đăng nhập tối thiểu 4 ký tự!");
-		}else if(isCustomerWithUsernameExit(customer.getUsername().trim())){
+		} else if (isCustomerWithUsernameExit(customer.getUsername().trim())) {
 			errors.rejectValue("username", "customer", "Tên đăng nhập đã tồn tại!");
 		}
 		if (customer.getFirstName().trim().length() == 0) {
@@ -130,13 +151,14 @@ public class GiftController {
 			ce.setFirstName(customer.getFirstName().trim());
 			ce.setLastName(customer.getLastName().trim());
 			ce.setTelephone(customer.getTelephone().trim());
-			ce.setAddress(customer.getSpecificAddress().trim()+", "+customer.getDistrict().trim()+", "+customer.getCity().trim());
+			ce.setAddress(customer.getSpecificAddress().trim() + ", " + customer.getDistrict().trim() + ", "
+					+ customer.getCity().trim());
 			ce.setEmail(customer.getEmail().trim());
 //			model.addAttribute("message", "Chúc mừng, bạn đã nhập đúng!");
-			if(insertCustomer(ce)) {
+			if (insertCustomer(ce)) {
 				System.out.println("Thêm thành công");
 				return "redirect:/store";
-			}else {
+			} else {
 				System.out.println("Thêm thất bại");
 				return "store/sign-up";
 			}
@@ -144,14 +166,24 @@ public class GiftController {
 	}
 
 	@RequestMapping("/category/{categoryId}")
-	public String category(ModelMap model, @PathVariable("categoryId") String categoryId) {
+	public String category(ModelMap model, @PathVariable("categoryId") String categoryId, HttpServletRequest request) {
 		model.addAttribute("list", getListProductBycategory(categoryId));
+		model.addAttribute("listCategory", getListCategory());
+		PagedListHolder pagedListHolder = new PagedListHolder(getListProductBycategory(categoryId));
+		int page = ServletRequestUtils.getIntParameter(request, "p", 0);
+		pagedListHolder.setPage(page);
+		pagedListHolder.setMaxLinkedPages(3);
+		pagedListHolder.setPageSize(8);
+		
+		model.addAttribute("pagedListHolder", pagedListHolder);
+
 		return "store/category-products";
 	}
 
 	@RequestMapping("/shopping-cart")
 	public String shoppingCart(ModelMap model) {
 		model.addAttribute("listCartDetail", getListCartDetail(getUserIdByUserName("tuanbui")));
+		model.addAttribute("listCategory", getListCategory());
 
 		for (CartDetailEntity c : getListCartDetail(getUserIdByUserName("tuanbui"))) {
 			System.out.println("user_id: " + c.getCustomer().getId() + "; product_id: " + c.getProduct().getId()
@@ -189,6 +221,7 @@ public class GiftController {
 
 	@RequestMapping(value = "/shopping-cart/delete/{productId}", method = RequestMethod.GET)
 	public String delete(@PathVariable("productId") String productId, ModelMap model) {
+		model.addAttribute("listCategory", getListCategory());
 		System.out.println(productId);
 		if (deleteProductFromCartDetail(productId)) {
 			System.out.println("Delete successful");
@@ -208,7 +241,8 @@ public class GiftController {
 	}
 
 	@RequestMapping(value = "/shopping-cart/update", method = RequestMethod.POST)
-	public String update(HttpServletRequest request) {
+	public String update(HttpServletRequest request, ModelMap model) {
+		model.addAttribute("listCategory", getListCategory());
 		String[] product_ids = request.getParameterValues("product_id");
 		String[] quantities = request.getParameterValues("quantity");
 		for (int i = 0; i < product_ids.length; i++) {
@@ -227,7 +261,8 @@ public class GiftController {
 	}
 
 	@RequestMapping("/shopping-cart/insert-into-cart/{productId}")
-	public String insertToCart(String customerId, @PathVariable("productId") String productId) {
+	public String insertToCart(String customerId, @PathVariable("productId") String productId, ModelMap model) {
+		model.addAttribute("listCategory", getListCategory());
 		Session session = factory.openSession();
 		Transaction t = session.beginTransaction();
 		CartDetailEntity cde = new CartDetailEntity(getUserIdByUserName("tuanbui"), (productId));
@@ -349,7 +384,15 @@ public class GiftController {
 		Query query = session.createQuery(hql);
 
 		CustomerEntity customer = (CustomerEntity) query.setParameter("username", username).uniqueResult();
-		return customer!=null;
+		return customer != null;
+	}
+
+	public String getPasswordOfCustomerWithUsername(String username) {
+		Session session = factory.getCurrentSession();
+		String hql = "SELECT c.password FROM CustomerEntity c WHERE c.username=:username";
+		Query query = session.createQuery(hql);
+		String password = (String) query.setParameter("username", username).uniqueResult();
+		return password;
 	}
 	
 	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
@@ -360,13 +403,14 @@ public class GiftController {
 		return matcher.find();
 	}
 
-	public static final Pattern VALID_VI_PHONE_NUMBER_REGEX = Pattern.compile("^0{1}[0-9]{9}$",Pattern.CASE_INSENSITIVE);
+	public static final Pattern VALID_VI_PHONE_NUMBER_REGEX = Pattern.compile("^0{1}[0-9]{9}$",
+			Pattern.CASE_INSENSITIVE);
 
 	public static boolean validatePhone(String phoneNumber) {
 		Matcher matcher = VALID_VI_PHONE_NUMBER_REGEX.matcher(phoneNumber);
 		return matcher.find();
 	}
-	
+
 	public String createTheNextCustomerId() {
 		Session session = factory.getCurrentSession();
 		String hql = "SELECT c.id FROM CustomerEntity c WHERE c.id=(SELECT MAX(cc.id) FROM CustomerEntity cc)";
@@ -378,5 +422,13 @@ public class GiftController {
 		String base2 = base.substring(0, base.length() - String.valueOf(x).length());
 		String newId = base2 + String.valueOf(x);
 		return newId;
+	}
+	public List<CategoryEntity> getListCategory() {
+		/* System.out.println("getListCartDetail"); */
+		Session session = factory.getCurrentSession();
+		String hql = "FROM CategoryEntity";
+		Query query = session.createQuery(hql);
+		List<CategoryEntity> list = query.list();
+		return list;
 	}
 }
