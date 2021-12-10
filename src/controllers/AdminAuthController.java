02f03aa542a9aa52c03ef711fc166909176015f7
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-10 08:12:20 +0700

    Finish forgot password and reset password functions

--- src/controllers/AdminAuthController.java
+++ src/controllers/AdminAuthController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,26 +12,27 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.validator.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import entities.AccountEntity;
 import entities.AdminEntity;
 import entities.CategoryEntity;
-import models.Account;
-import models.Login;
+import models.AdminResetPasswordModel;
+import models.EntityData;
+import models.Mailer;
+import models.dao.AccountDAO;
+import models.dao.DAO;
+import models.validator.InputValidator;
 
 @Transactional
 @Controller
@@ -42,6 +42,11 @@ public class AdminAuthController {
 	@Qualifier("sessionFactory")
 	SessionFactory factory;
 
+	@Autowired
+	Mailer mailer;
+
+	EntityData entityData;
+
 	String viewsDirectory = "admin/pages/account/";
 
 	@RequestMapping(value = "login", method = RequestMethod.GET)
@@ -50,19 +55,18 @@ public class AdminAuthController {
 		if (session.getAttribute("admin") != null) {
 			return "redirect:/admin/dashboard";
 		}
-
-		model.addAttribute("account", new AccountEntity());
-		model.addAttribute("title", "Admin Đăng nhập");
-		return viewsDirectory + "login";
+		else {
+			model.addAttribute("account", new AccountEntity());
+			model.addAttribute("title", "Admin Đăng nhập");
+			return viewsDirectory + "login";
+		}
 	}
 
 	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String login(RedirectAttributes redirectAttributes, ModelMap model, HttpServletRequest request,
-			@Validated @ModelAttribute("account") AccountEntity account, BindingResult errors) {
-		/*
-		 * String username = request.getParameter("username"); String password =
-		 * request.getParameter("password");
-		 */
+	public String login(RedirectAttributes redirectAttributes, ModelMap model, HttpSession session,
+			@Validated @ModelAttribute("account") AccountEntity login, BindingResult errors) {
+
+		entityData = new EntityData(factory);
 
 		if (errors.hasErrors()) {
 			if (errors.hasFieldErrors("username")) {
@@ -79,24 +83,20 @@ public class AdminAuthController {
 
 			return viewsDirectory + "login";
 		} else {
-			String username = account.getUsername();
-			String password = account.getPassword();
+			String username = login.getUsername();
+			String password = login.getPassword();
 
-			Session session = factory.getCurrentSession();
-			String hql = "FROM AccountEntity WHERE username = '" + username + "' AND password = '" + password + "'";
-			Query query = session.createQuery(hql);
+			AccountEntity account = entityData.getAccount(username, password);
 
-			if (!query.list().isEmpty()) {
-				AccountEntity admin = (AccountEntity) query.list().get(0);
-				HttpSession session2 = request.getSession();
-				session2.setAttribute("admin", admin.getId());
+			if (account != null) {
+				session.setAttribute("adminId", account.getId());
+				// session.setAttribute("admin", account.getAdmin());
 				return "redirect:/admin/dashboard";
-				// return new RedirectView(request.getContextPath() + "/admin/dashboard");
-			}
-
-			else {
+			} else {
 				model.addAttribute("message", "Tên đăng nhập hoặc mật khẩu không đúng");
+				model.addAttribute("messageType", "error");
 				model.addAttribute("title", "Admin Đăng nhập");
+				model.addAttribute("account", login);
 				return viewsDirectory + "login";
 			}
 		}
@@ -109,11 +109,12 @@ public class AdminAuthController {
 
 	@RequestMapping(value = "register", method = RequestMethod.POST)
 	public String register() {
-		String id = generateId(5);
+		String id = generateRecoveryCode(5);
 
 		return "redirect:/admin/login";
 	}
 
+	// FORGOT PASSWORD
 	@RequestMapping("forgot-password")
 	public String renderForgotPasswordPage(ModelMap model) {
 		AdminEntity admin = new AdminEntity();
@@ -124,8 +125,8 @@ public class AdminAuthController {
 	}
 
 	@RequestMapping(value = "forgot-password", method = RequestMethod.POST)
-	public String getRecoveryCode(ModelMap model, HttpServletRequest request,
-			@ModelAttribute("admin") AdminEntity admin, BindingResult errors) {
+	public String getRecoveryCode(ModelMap model, HttpServletRequest request, HttpSession session,
+			@ModelAttribute("admin") AdminEntity admin, BindingResult errors, RedirectAttributes redirectAttributes) {
 
 		int errorCount = 0;
 		if (admin.getId().isEmpty()) {
@@ -146,71 +147,149 @@ public class AdminAuthController {
 			errorCount++;
 		}
 
-		if (errors.hasErrors()) {
+		if (errorCount != 0) {
 			model.addAttribute("admin", admin);
-			model.addAttribute("title", "Admin Lost Password");
+			model.addAttribute("title", "Quên mật khẩu");
 			return viewsDirectory + "forgot-password";
 		}
 
 		else {
-			Session session = factory.openSession();
-			String hql = "FROM AdminEntity WHERE account.username = '" + admin.getAccount().getUsername() + "' AND id = '" + admin.getId() + "' AND email = '" + admin.getEmail() + "'";
-			Query query = session.createQuery(hql);
-			System.out.println(query.list().size());
-			//session.close();
+			AccountDAO dao = new AccountDAO(factory);
+			AccountEntity account = dao.getAccount(admin.getId(), admin.getAccount().getUsername(), admin.getEmail());
+			if (account != null) {
+				session.setAttribute("adminId", account.getId());
+				session.setMaxInactiveInterval(1000 * 60 * 5);
+				String recoveryCode = generateRecoveryCode(5);
+				account.setRecovery(recoveryCode);
+				dao.updateAccount(account);
 
-			if (!query.list().isEmpty()) {
-				AdminEntity admin2 = (AdminEntity) query.list().get(0);
-				admin2.getAccount().setRecovery(generateId(5));
-				updateDB(admin2);
-				return viewsDirectory + "reset-password";
+				mailer.send(account.getAdmin().getEmail(), "Mã khôi phục mật khẩu", recoveryCode);
+				return "redirect:/admin/reset-password";
 			} else {
-				return "redirect:/admin/login";
+				redirectAttributes.addFlashAttribute("message", "Không tìm thấy Admin!");
+				redirectAttributes.addFlashAttribute("messageType", "error");
+				return "redirect:/admin/reset-password";
 			}
 		}
 	}
 
+	// RESET PASSWORD
+	@RequestMapping(value = "reset-password", method = RequestMethod.GET)
+	public String renderResetPasswordPage(ModelMap model, HttpSession session) {
+		String adminId = null;
+		try {
+			adminId = session.getAttribute("adminId").toString();
+			if (adminId != null) {
+				AdminResetPasswordModel resetPassword = new AdminResetPasswordModel();
+				model.addAttribute("reset-password", resetPassword);
+				model.addAttribute("title", "Khôi phục mật khẩu");
+				return viewsDirectory + "reset-password";
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/admin/forgot-password";
+	}
+
+	@RequestMapping(value = "reset-password", method = RequestMethod.POST)
+	public String resetPassword(ModelMap model, HttpServletRequest request, HttpSession session,
+			@ModelAttribute("reset-password") AdminResetPasswordModel resetPassword, BindingResult errors,
+			RedirectAttributes redirectAttributes) {
+
+		AccountDAO dao = new AccountDAO(factory);
+		try {
+			String adminId = session.getAttribute("adminId").toString();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "redirect:/admin/forgot-password";
+		}
+
+		int errorCount = 0;
+		if (!InputValidator.isInputValid(resetPassword.getRecoveryCode())) {
+			errors.rejectValue("recoveryCode", "reset-password", "Xin hãy nhập mã khôi phục!");
+			model.addAttribute("recoveryCodeValid", "is-invalid");
+			errorCount++;
+		}
+		if (!InputValidator.isInputValid(resetPassword.getNewPassword())) {
+			errors.rejectValue("newPassword", "reset-password", "Xin hãy nhập mật khẩu mới!");
+			model.addAttribute("newPasswordValid", "is-invalid");
+			errorCount++;
+		}
+		if (!InputValidator.isInputValid(resetPassword.getRePassword())) {
+			errors.rejectValue("rePassword", "reset-password", "Xin hãy nhập lại mật khẩu mới!");
+			model.addAttribute("rePasswordValid", "is-invalid");
+			errorCount++;
+		} else if (!resetPassword.getRePassword().trim().equals(resetPassword.getNewPassword().trim())) {
+			errors.rejectValue("rePassword", "reset-password", "Mật khẩu không trùng khớp");
+			model.addAttribute("rePasswordValid", "is-invalid");
+			errorCount++;
+		}
+		if (errorCount != 0) {
+			model.addAttribute("reset-password", resetPassword);
+			model.addAttribute("message", "Xin hãy nhập đúng thông tin");
+			model.addAttribute("messageType", "error");
+			model.addAttribute("title", "Khôi phục mật khẩu");
+			return viewsDirectory + "reset-password";
+		}
+
+		else {
+			String adminId = null;
+			try {
+				adminId = session.getAttribute("adminId").toString();
+				AccountEntity account = dao.getAccount(adminId, resetPassword.getRecoveryCode());
+
+				/*
+				 * if (admin.getAccount().getRecovery().equals(resetPassword.getRecoveryCode()))
+				 * { admin.getAccount().setPassword(resetPassword.getNewPassword());
+				 * admin.getAccount().setRecovery(""); updateAccount(admin); }
+				 */
+				if (account != null) {
+					account.setPassword(resetPassword.getNewPassword());
+					account.setRecovery("");
+					dao.updateAccount(account);
+					redirectAttributes.addFlashAttribute("message", "Khôi phục mật khẩu thành công!");
+					redirectAttributes.addFlashAttribute("messageType", "success");
+					return "redirect:/admin/login";
+				} else {
+					redirectAttributes.addFlashAttribute("message", "Mã khôi phục không trùng khớp!");
+					redirectAttributes.addFlashAttribute("messageType", "error");
+					return "redirect:/admin/reset-password";
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				redirectAttributes.addFlashAttribute("message", "Mã khôi phục đã hết hạn!");
+				redirectAttributes.addFlashAttribute("messageType", "error");
+				return "redirect:/admin/forgot-password";
+			}
+		}
+	}
+
+	// LOGOUT
 	@RequestMapping("logout")
 	public String logout(HttpServletRequest request) throws IOException {
 		HttpSession session = request.getSession();
 		session.setAttribute("admin", null);
 		return "redirect:/admin/login";
 	}
-	
-	public void updateDB(Object entity) {
+
+	public AdminEntity getAdmin(String id) {
+
 		Session session = factory.openSession();
-		Transaction t = session.beginTransaction();
-		try {
-			session.merge(entity);
-			t.commit();
-			System.out.println("Updated");
-
-		} catch (Exception e) {
-			t.rollback();
-			System.out.println("Error");
-			e.printStackTrace();
-		} finally {
-			session.close();
-		}
+		String hql = "FROM AdminEntity WHERE id = '" + id + "'";
+		Query query = session.createQuery(hql);
+		AdminEntity admin = (AdminEntity) query.list().get(0);
+		session.close();
+		return admin;
 	}
-	
-	/*
-	 * public AdminInfoEntity getAdmin(AdminInfoEntity admin) { Session session =
-	 * factory.openSession(); String hql = "FROM CategoryEntity WHERE id = '" + id +
-	 * "'"; Query query = session. CategoryEntity category = (CategoryEntity)
-	 * query.list().get(0); session.close(); return category; }
-	 */
-	
 
-	public static String generateId(int targetStringLength) {
+	public static String generateRecoveryCode(int targetStringLength) {
 		int leftLimit = 48; // numeral '0'
 		int rightLimit = 57; // numeral '9'
 		Random random = new Random();
 
 		String generatedString = random.ints(leftLimit, rightLimit + 1)
-				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength - 1)
+				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
 				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
-
-		return "A" + generatedString;
+		return generatedString;
 	}
 }
