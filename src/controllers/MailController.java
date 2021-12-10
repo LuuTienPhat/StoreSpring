228714a2005329edb82623d09a3b1e6220122253
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-10 22:50:16 +0700

    Add new mapping but firing when receive post request,
    receive recipience's email address, display that email to input box

--- src/controllers/MailController.java
+++ src/controllers/MailController.java
@@ -10,16 +10,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/mail")
 public class MailController {
 	@Autowired
 	JavaMailSender mailer;
-
-	/*
-	 * @Autowired UploadFile uploadFile;
-	 */
 
 	String viewsDirectory = "admin/pages/mail/";
 
@@ -29,26 +26,35 @@ public class MailController {
 		return viewsDirectory + "mail";
 	}
 
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String renderMailPageWithEmail(ModelMap model,
+			@RequestParam(value = "email", required = true) String email) {
+		model.addAttribute("title", "Gửi mail");
+		model.addAttribute("email", email);
+		return viewsDirectory + "mail";
+	}
+
 	@RequestMapping(value = "/send", method = RequestMethod.POST)
 	public String send(ModelMap model, @RequestParam("email") String email, @RequestParam("subject") String subject,
-			@RequestParam("body") String body) {
+			@RequestParam("body") String body, RedirectAttributes redirectAttributes) {
 
 		try {
-
 			MimeMessage mail = mailer.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(mail);
-			//helper.setFrom("");
+			// helper.setFrom("");
 			helper.setTo(email);
-			//helper.setReplyTo("");
+			// helper.setReplyTo("");
 			helper.setSubject(subject);
 			helper.setText(body, true);
 
 			mailer.send(mail);
-			model.addAttribute("message", "Email sent successfully!");
+			redirectAttributes.addFlashAttribute("message", "Gửi Email thành công!");
+			redirectAttributes.addFlashAttribute("messageType", "success");
 
 		} catch (Exception e) {
 			// TODO: handle exception
-			model.addAttribute("message", "Unable to send email");
+			redirectAttributes.addFlashAttribute("message", "Không thể gửi email");
+			redirectAttributes.addFlashAttribute("messageType", "error");
 			e.printStackTrace();
 		}
 
