b0e8e9466a216f1429285e61eda22d498ea37c1f
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-15 22:11:45 +0700

    Organize imports, add register mapping and add new admin to db.

--- src/controllers/AdminAuthController.java
+++ src/controllers/AdminAuthController.java
@@ -1,20 +1,17 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -22,16 +19,20 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import entities.AccountEntity;
 import entities.AdminEntity;
-import entities.CategoryEntity;
 import models.AdminResetPasswordModel;
-import models.EntityData;
+import models.Generate;
 import models.Mailer;
+import models.UploadFile;
+import models.List.Admins;
 import models.dao.AccountDAO;
-import models.dao.DAO;
+import models.dao.AdminDAO;
+import models.validator.DateTimeValidator;
 import models.validator.InputValidator;
 
 @Transactional
@@ -39,16 +40,25 @@ import models.validator.InputValidator;
 @RequestMapping("/admin/")
 public class AdminAuthController {
 	@Autowired
+	ServletContext context;
+
+	@Autowired
 	@Qualifier("sessionFactory")
 	SessionFactory factory;
 
 	@Autowired
 	Mailer mailer;
 
-	EntityData entityData;
+	@Autowired
+	@Qualifier("uploadFile")
+	UploadFile uploadFile;
+
+	AdminDAO adminDAO;
+	AccountDAO accountDAO;
 
 	String viewsDirectory = "admin/pages/account/";
 
+	// LOGIN
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String renderLoginPage(HttpServletRequest request, ModelMap model) {
 		HttpSession session = request.getSession();
@@ -60,7 +70,7 @@ public class AdminAuthController {
 		} catch (Exception e) {
 			// TODO: handle exception
 			model.addAttribute("account", new AccountEntity());
-			model.addAttribute("title", "Admin Đăng nhập");
+			model.addAttribute("title", "Đăng nhập");
 			return viewsDirectory + "login";
 
 		}
@@ -71,7 +81,8 @@ public class AdminAuthController {
 	public String login(RedirectAttributes redirectAttributes, ModelMap model, HttpSession session,
 			@Validated @ModelAttribute("account") AccountEntity login, BindingResult errors) {
 
-		entityData = new EntityData(factory);
+		adminDAO = new AdminDAO(factory);
+		accountDAO = new AccountDAO(factory);
 
 		if (errors.hasErrors()) {
 			if (errors.hasFieldErrors("username")) {
@@ -91,7 +102,7 @@ public class AdminAuthController {
 			String username = login.getUsername();
 			String password = login.getPassword();
 
-			AccountEntity account = entityData.getAccount(username, password);
+			AccountEntity account = accountDAO.getAccountAuth(username, password);
 
 			if (account != null) {
 				session.setAttribute("adminId", account.getId());
@@ -100,23 +111,155 @@ public class AdminAuthController {
 			} else {
 				model.addAttribute("message", "Tên đăng nhập hoặc mật khẩu không đúng");
 				model.addAttribute("messageType", "error");
-				model.addAttribute("title", "Admin Đăng nhập");
+				model.addAttribute("title", "Đăng nhập");
 				model.addAttribute("account", login);
 				return viewsDirectory + "login";
 			}
 		}
 	}
 
+	// REGISTER
 	@RequestMapping(value = "register", method = RequestMethod.GET)
-	public String renderSignUpPage() {
+	public String renderSignUpPage(ModelMap model, HttpSession session) {
+		AdminEntity admin = new AdminEntity();
+		model.addAttribute("newAdmin", admin);
+		model.addAttribute("title", "Đăng ký");
 		return viewsDirectory + "register";
 	}
 
 	@RequestMapping(value = "register", method = RequestMethod.POST)
-	public String register() {
-		String id = generateRecoveryCode(5);
+	public String updateProfile(ModelMap model, HttpSession session, HttpServletRequest request,
+			@RequestParam(value = "image", required = false) MultipartFile image,
+			@RequestParam(value = "cover", required = false) MultipartFile cover,
+			@ModelAttribute("newAdmin") AdminEntity newAdmin, BindingResult errors,
+			RedirectAttributes redirectAttributes) throws IOException, InterruptedException {
 
-		return "redirect:/admin/login";
+		int errorsCount = 0;
+		adminDAO = new AdminDAO(factory);
+		Admins admins = adminDAO.getAdmins();
+
+		if (!InputValidator.isFirstnameValid(newAdmin.getFirstname().trim())) {
+			errors.rejectValue("firstname", "admin", "Tên không hợp lệ");
+			model.addAttribute("firstnameValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isInputValid(newAdmin.getLastname())) {
+			errors.rejectValue("lastname", "admin", "Họ không hợp lệ");
+			model.addAttribute("lastnameValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!DateTimeValidator.isDateValid(newAdmin.getBirthday())) {
+			errors.rejectValue("birthday", "admin", "Ngày sinh không hợp lệ");
+			model.addAttribute("birthdayValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isGenderValid(newAdmin.getGender())) {
+			errors.rejectValue("gender", "admin", "Giới tính không hợp lệ");
+			model.addAttribute("genderValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isPhoneValid(newAdmin.getPhone())) {
+			errors.rejectValue("phone", "admin", "Số điện thoại không hợp lệ");
+			model.addAttribute("phoneValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isEmailValid(newAdmin.getEmail())) {
+			errors.rejectValue("email", "admin", "Email không hợp lệ");
+			model.addAttribute("emailValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isInputValid(newAdmin.getAccount().getUsername())) {
+			errors.rejectValue("account.username", "admin", "Tên đăng nhập không hợp lệ");
+			model.addAttribute("usernameValid", "is-invalid");
+			errorsCount++;
+		} else if (Admins.isUserNameExist(admins, newAdmin.getAccount().getUsername())) {
+			errors.rejectValue("account.username", "admin", "Tên đăng nhập không hợp lệ");
+			model.addAttribute("usernameValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isInputValid(newAdmin.getAccount().getPassword())) {
+			errors.rejectValue("account.password", "admin", "Mật khẩu không hợp lệ");
+			model.addAttribute("passwordValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (!InputValidator.isInputValid(request.getParameter("rePassword"))) {
+			model.addAttribute("rePasswordValid", "is-invalid");
+			errorsCount++;
+		}
+
+		if (errorsCount != 0) {
+			model.addAttribute("newAdmin", newAdmin);
+			model.addAttribute("title", "Đăng ký");
+			model.addAttribute("message", "Vui lòng điền đầy đủ thông tin!");
+			model.addAttribute("messageType", "warning");
+			return viewsDirectory + "register";
+
+		} else {
+			adminDAO = new AdminDAO(factory);
+			String adminId = Generate.generateAdminId(adminDAO.getAdmins().getList());
+			newAdmin.setId(adminId);
+
+			File folderInServer = new File(
+					uploadFile.getUploadPathOnServer(context) + "admins/" + newAdmin.getId() + "/");
+			File folderInResource = new File(uploadFile.getUploadPath() + "admins/" + newAdmin.getId() + "/");
+
+			if (!folderInResource.exists())
+				folderInResource.mkdir();
+			if (!folderInServer.exists())
+				folderInServer.mkdir();
+
+			if (image.getSize() != 0) {
+				String imageFileName = "image" + UploadFile.getExtension(image.getOriginalFilename());
+				String imagePath = uploadFile.getRelativeUploadPath() + "admins/" + newAdmin.getId() + "/"
+						+ imageFileName;
+				newAdmin.setImage(imagePath);
+
+				File fileInServer = new File(
+						uploadFile.getUploadPathOnServer(context) + "admins/" + newAdmin.getId() + "/" + imageFileName);
+				File fileInResource = new File(
+						uploadFile.getUploadPath() + "admins/" + newAdmin.getId() + "/" + imageFileName);
+
+				UploadFile.writeFile(fileInServer, image);
+				UploadFile.writeFile(fileInResource, image);
+
+				Thread.sleep(2000);
+			}
+
+			if (cover.getSize() != 0) {
+				String imageFileName = "cover" + UploadFile.getExtension(cover.getOriginalFilename());
+				String imagePath = uploadFile.getRelativeUploadPath() + "admins/" + newAdmin.getId() + "/"
+						+ imageFileName;
+				newAdmin.setCover(imagePath);
+
+				File fileInServer = new File(
+						uploadFile.getUploadPathOnServer(context) + "admins/" + newAdmin.getId() + "/" + imageFileName);
+				File fileInResource = new File(
+						uploadFile.getUploadPath() + "admins/" + newAdmin.getId() + "/" + imageFileName);
+
+				UploadFile.writeFile(fileInServer, cover);
+				UploadFile.writeFile(fileInResource, cover);
+
+				Thread.sleep(2000);
+			}
+
+			if (adminDAO.updateAdminInDB(newAdmin)) {
+				redirectAttributes.addFlashAttribute("message", "Thêm quản trị viên thành công!");
+				redirectAttributes.addFlashAttribute("messageType", "success");
+			} else {
+				redirectAttributes.addFlashAttribute("message", "Không thể thêm quản trị viên");
+				redirectAttributes.addFlashAttribute("messageType", "error");
+			}
+		}
+
+		return "redirect:/admin/profile";
 	}
 
 	// FORGOT PASSWORD
@@ -125,7 +268,7 @@ public class AdminAuthController {
 		AdminEntity admin = new AdminEntity();
 
 		model.addAttribute("admin", admin);
-		model.addAttribute("title", "Admin Lost Password");
+		model.addAttribute("title", "AdminDAO Lost Password");
 		return viewsDirectory + "forgot-password";
 	}
 
@@ -171,7 +314,7 @@ public class AdminAuthController {
 				mailer.send(account.getAdmin().getEmail(), "Mã khôi phục mật khẩu", recoveryCode);
 				return "redirect:/admin/reset-password";
 			} else {
-				redirectAttributes.addFlashAttribute("message", "Không tìm thấy Admin!");
+				redirectAttributes.addFlashAttribute("message", "Không tìm thấy AdminDAO!");
 				redirectAttributes.addFlashAttribute("messageType", "error");
 				return "redirect:/admin/reset-password";
 			}
@@ -243,11 +386,6 @@ public class AdminAuthController {
 				adminId = session.getAttribute("adminId").toString();
 				AccountEntity account = dao.getAccount(adminId, resetPassword.getRecoveryCode());
 
-				/*
-				 * if (admin.getAccount().getRecovery().equals(resetPassword.getRecoveryCode()))
-				 * { admin.getAccount().setPassword(resetPassword.getNewPassword());
-				 * admin.getAccount().setRecovery(""); updateAccount(admin); }
-				 */
 				if (account != null) {
 					account.setPassword(resetPassword.getNewPassword());
 					account.setRecovery("");
@@ -277,16 +415,6 @@ public class AdminAuthController {
 		session.removeAttribute("adminId");
 		session.removeAttribute("admin");
 		return "redirect:/admin/login";
-	}
-
-	public AdminEntity getAdmin(String id) {
-
-		Session session = factory.openSession();
-		String hql = "FROM AdminEntity WHERE id = '" + id + "'";
-		Query query = session.createQuery(hql);
-		AdminEntity admin = (AdminEntity) query.list().get(0);
-		session.close();
-		return admin;
 	}
 
 	public static String generateRecoveryCode(int targetStringLength) {
