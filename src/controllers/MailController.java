4171b9a320f47c5b15f55cb6e87525b973004426
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-18 23:38:56 +0700

    Add model attribute to page, check input before sending mail

--- src/controllers/MailController.java
+++ src/controllers/MailController.java
@@ -1,16 +1,19 @@
 package controllers;
 
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import models.MailModel;
+import models.Mailer;
+import models.validator.InputValidator;
 
 @Controller
 @RequestMapping("/admin/mail")
@@ -23,41 +26,67 @@ public class MailController {
 	@RequestMapping("")
 	public String renderMailPage(ModelMap model) {
 		model.addAttribute("title", "Gửi mail");
+		model.addAttribute("mail", new MailModel());
 		return viewsDirectory + "mail";
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public String renderMailPageWithEmail(ModelMap model,
 			@RequestParam(value = "email", required = true) String email) {
+
+		MailModel mail = new MailModel();
+		mail.setEmail(email);
+
+		model.addAttribute("mail", mail);
 		model.addAttribute("title", "Gửi mail");
-		model.addAttribute("email", email);
 		return viewsDirectory + "mail";
 	}
 
 	@RequestMapping(value = "/send", method = RequestMethod.POST)
-	public String send(ModelMap model, @RequestParam("email") String email, @RequestParam("subject") String subject,
-			@RequestParam("body") String body, RedirectAttributes redirectAttributes) {
+	public String send(ModelMap model, @ModelAttribute("mail") MailModel mailModel, BindingResult errors,
+			RedirectAttributes redirectAttributes) {
 
-		try {
-			MimeMessage mail = mailer.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(mail);
-			// helper.setFrom("");
-			helper.setTo(email);
-			// helper.setReplyTo("");
-			helper.setSubject(subject);
-			helper.setText(body, true);
+		int errorCount = 0;
 
-			mailer.send(mail);
-			redirectAttributes.addFlashAttribute("message", "Gửi Email thành công!");
-			redirectAttributes.addFlashAttribute("messageType", "success");
-
-		} catch (Exception e) {
-			// TODO: handle exception
-			redirectAttributes.addFlashAttribute("message", "Không thể gửi email");
-			redirectAttributes.addFlashAttribute("messageType", "error");
-			e.printStackTrace();
+		if (!InputValidator.isInputValid(mailModel.getEmail())) {
+			errors.rejectValue("email", "mail", "Nhập địa chỉ email nhận");
+			model.addAttribute("emailValid", "is-invalid");
+			errorCount++;
+		} else if (!InputValidator.isEmailValid(mailModel.getEmail())) {
+			errors.rejectValue("email", "mail", "Email không hợp lệ!");
+			model.addAttribute("emailValid", "is-invalid");
+			errorCount++;
 		}
 
-		return "redirect:/admin/mail";
+		if (!InputValidator.isInputValid(mailModel.getSubject())) {
+			errors.rejectValue("subject", "mail", "Nhập tựa đề mail");
+			model.addAttribute("subjectValid", "is-invalid");
+			errorCount++;
+		}
+
+		if (!InputValidator.isInputValid(mailModel.getBody())) {
+			errors.rejectValue("body", "mail", "Nhập nội dung mail");
+			model.addAttribute("bodyValid", "is-invalid");
+			errorCount++;
+		}
+
+		if (errorCount != 0) {
+			model.addAttribute("message", "Xin hãy nhập đầy đủ thông tin!");
+			model.addAttribute("messageType", "error");
+			model.addAttribute("mail", mailModel);
+			model.addAttribute("title", "Gửi mail");
+			return viewsDirectory + "mail";
+		} else {
+			Mailer mailer = new Mailer();
+
+			if (mailer.send(mailModel.getEmail(), mailModel.getSubject(), mailModel.getBody())) {
+				redirectAttributes.addFlashAttribute("message", "Gửi Email thành công!");
+				redirectAttributes.addFlashAttribute("messageType", "success");
+			} else {
+				redirectAttributes.addFlashAttribute("message", "Không thể gửi email");
+				redirectAttributes.addFlashAttribute("messageType", "error");
+			}
+			return "redirect:/admin/mail";
+		}
 	}
 }
